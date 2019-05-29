e758804acd737e5173a75c278e658c3558ec5015
Aqil Thanawala <aqil.thanawala@gmail.com>
2019-05-28 17:34:48 -0700

    Remove notesData.js file, notes now come from postgres

--- main-app/api/server/routes/notes.js
+++ main-app/api/server/routes/notes.js
@@ -1,15 +1,16 @@
 const passport = require('passport');
-//  const knex = require('../config/knex_config.js');
-const notes = require('./notesData');
+const knex = require('../config/knex_config.js');
 
 const jwtAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = (app) => {
   //  get all notes by a certain participant
   app.get('/notes/:participantId', jwtAuth, (req, res) => {
-    //  knex??
-    console.log(notes);
-    res.status(200).send(notes);
+    const { participantId: id } = req.params;
+
+    knex('notes').where('participant_id', id).select()
+      .then(notes => res.status(200).send(notes))
+      .catch(err => res.status(500).send(err));
   });
 
   //  post a new note to the participant
@@ -23,20 +24,20 @@ module.exports = (app) => {
   app.get('/note/:noteId', jwtAuth, (req, res) => {
     // knex???
     console.log('response: note here');
-    res.status(200).send(notes[0]);
+    res.status(200).send(['hi']);
   });
 
   //  put to edit an individual note by note id
   app.put('/note/:noteId', jwtAuth, (req, res) => {
     // knex???
     console.log('response: note here');
-    res.status(200).send(notes[0]);
+    res.status(200).send(['hi']);
   });
 
   //  patch an individual note by note id to change it to closed status
   app.patch('/note/:noteId', jwtAuth, (req, res) => {
     // knex???
     console.log('response: note here');
-    res.status(200).send(notes[0]);
+    res.status(200).send(['hi']);
   });
 };
